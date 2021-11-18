97844238751f46b38ee596932e48c6a1cf5659d2
LuiCoro <luiacorona029@icloud.com>
2021-11-17 23:24:04 -0800

    'Step 01' - Creating some Dummy Data for our tables

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
-// STRETCH
+
+const cars = [
+  {
+    vin: '0000000000001',
+    make: 'Lexus',
+    model: 'RC300',
+    mileage: 50000,
+    title: 'salvage',
+    transmission: 'manuel',
+  },
+  {
+    vin: '0000000000002',
+    make: 'Toyota',
+    model: 'Tacoma',
+    mileage: 20000,
+    title: 'clean',
+  },
+  {
+    vin: '0000000000003',
+    make: 'Honda',
+    model: 'Type R',
+    mileage: 1000,
+  },
+]
+
+exports.seed = async function (knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert(cars)
+}
