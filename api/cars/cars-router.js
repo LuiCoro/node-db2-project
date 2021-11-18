1ccbd531657742ec5384a7163811d59d8223b478
LuiCoro <luiacorona029@icloud.com>
2021-11-18 00:31:51 -0800

    'Step 05' - Setting up basic skeleton for Router - imported it to our server.js - Our Routes are connected correctly - NEED to figure out how to DEBUG correctly in InteliJ

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,19 @@
-// DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  res.status(200).json('GET is CONNECTED! set it up!')
+})
+
+
+router.get('/:id', async (req, res) => {
+  res.status(200).json('GET is CONNECTED! set it up!')
+})
+
+router.post('/', async (req, res) => {
+  res.status(200).json('GET is CONNECTED! set it up!')
+})
+
+
+module.exports = router
