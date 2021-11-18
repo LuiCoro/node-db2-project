d39f2acb8bca4b11433895ffa955a98ac68ed721
LuiCoro <luiacorona029@icloud.com>
2021-11-18 01:21:39 -0800

    'Step 06' - Setting up our Models - Imported our DB Config Files - COMPLETED .getALl() - CONNECTED -> cars-router.js ' CARS-ROUTER.js ' [PRE-MIDDLEWARE] - ABle to get a list of all cars

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const Cars = require('./cars-model')
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  res.status(200).json('GET is CONNECTED! set it up!')
+router.get('/', async (req, res, next) => {
+  try{
+    const cars = await Cars.getAll()
+    res.status(200).json(cars)
+  } catch (err) {
+    next(err)
+  }
 })
 
 
